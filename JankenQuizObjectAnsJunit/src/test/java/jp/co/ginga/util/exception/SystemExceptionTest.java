80e1ef6c09d4111f70e3e5e572909587d3911361
n_kyo <n_kyo@HOME-PC>
2022-09-19 12:57:35 +0900

    SystemExceptionクラスのMockテスト:Complete

--- JankenQuizObjectAnsJunit/src/test/java/jp/co/ginga/util/exception/SystemExceptionTest.java
+++ JankenQuizObjectAnsJunit/src/test/java/jp/co/ginga/util/exception/SystemExceptionTest.java
@@ -5,49 +5,62 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.lang.reflect.Field;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 
 public class SystemExceptionTest {
+	
 	//テストデータ
-		private String message = "エラーが発生しました";
+	private String message = "エラーが発生しました";
 
-		@InjectMocks
-		private SystemException exception = new SystemException(message);
-
-		/**
-		 * testConstructor001 正常系
-		 * public SystemException(String sysMsg)
-		 * 引数の値がフィールドに代入されることを確認
-		 */
-		@Test
-		public void testConstructor001() {
-			try {
-				Field sysMsgField = SystemException.class.getDeclaredField("sysMsg");
-				sysMsgField.setAccessible(true);
-				//テストメソッド
-				SystemException e = new SystemException(this.message);
-
-				//検証
-				String result = String.valueOf(sysMsgField.get(e));
-
-				assertEquals(this.message, result);
-
-			}catch(NoSuchFieldException | SecurityException | NullPointerException| IllegalArgumentException | IllegalAccessException  e) {
-				e.printStackTrace();
-				fail();
-			}
-		}
-
-		/**
-		 * testConstructor001 正常系
-		 * public String getSysMsg()
-		 * フィールドの値が取り出されることを確認する
-		 */
-		@Test
-		public void testGetSysMsg001() {
+	
+	/**
+	 * testConstructor001 正常系
+	 * public SystemException(String sysMsg)
+	 * --確認事項--
+	 * インスタンス生成時に引数として渡したデータがsysMsgフィールドに代入されているか
+	 * --条件--
+	 * 引数は文字列
+	 * --検証項目--
+	 * 1. sysMsgフィールドに引数が代入されている
+	 */
+	@Test
+	public void testConstructor001() {
+		try {
+			//準備
+			Field sysMsgField = SystemException.class.getDeclaredField("sysMsg");
+			sysMsgField.setAccessible(true);
+			
 			//テストメソッド
-			String result = this.exception.getSysMsg();
+			SystemException e = new SystemException(this.message);
+
 			//検証
+			String result = (String) sysMsgField.get(e);
 			assertEquals(this.message, result);
+			
+		}catch(Exception  e) {
+			e.printStackTrace();
+			fail();
 		}
+	}
+
+	/**
+	 * testGetSysMsg001 正常系
+	 * public String getSysMsg()
+	 * --確認事項--
+	 * sysMsgフィールドの値が返されるか
+	 * --条件--
+	 * 引数は文字列
+	 * --検証項目--
+	 * 1. sysMsgフィールドの値が返されているか
+	 */
+	@Test
+	public void testGetSysMsg001() {
+		//準備
+		SystemException e = new SystemException(this.message);
+		
+		//テストメソッド
+		String result = e.getSysMsg();
+		
+		//検証
+		assertEquals(this.message, result);
+	}
 }
