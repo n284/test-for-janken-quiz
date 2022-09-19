12337affff55407322a1f599a880c55cbc7ead99
n_kyo <n_kyo@HOME-PC>
2022-09-19 22:08:35 +0900

    QuizCuiGameApplicationImplクラスのMockテスト:Complete

--- JankenQuizObjectAnsJunit/src/test/java/jp/co/ginga/cui/impl/quiz/QuizCuiGameApplicationImplTest.java
+++ JankenQuizObjectAnsJunit/src/test/java/jp/co/ginga/cui/impl/quiz/QuizCuiGameApplicationImplTest.java
@@ -34,8 +34,6 @@ public class QuizCuiGameApplicationImplTest{
 	private int two = 2;
 	private int three = 3;
 	private String alphabet = "a";
-
-
 	private List<QuizQuestion> quizList = new ArrayList<QuizQuestion>();
 	private List<QuizQuestion> emptyQuizList = new ArrayList<QuizQuestion>();
 	private String errorMessage ;
@@ -67,17 +65,28 @@ public class QuizCuiGameApplicationImplTest{
 	/**
 	 * testAction001 正常系
 	 * public void action() throws SystemException
-	 * SystemExceptionも含めて例外が発生しないことを確認
+	 * --確認事項--
+	 * 実行中に例外が発生しないこと
+	 * getIntが指定した値が戻されるようにmock化
+	 * --条件--
+	 * mock化したgetIntの戻り値は1回目は1、2回目は2、3回目は3
+	 * --検証項目--
+	 * 1. 例外が発生せず処理を終えること
+	 * 2. getIntが3回呼び出されるか
 	 */
 	@Test
 	public void testAction001() {
 		//モック化
 		try(MockedStatic<Keybord> mockKeybord = mockStatic(Keybord.class)){
-			//解答、1, 2, 3
-			mockKeybord.when(() -> Keybord.getInt(this.one, this.three)).thenReturn(this.one).thenReturn(this.two).thenReturn(this.one).thenReturn(this.three);
-
+			//入力値 1, , 2, 3
+			mockKeybord.when(() -> Keybord.getInt(this.one, this.three)).thenReturn(this.one).thenReturn(this.two).thenReturn(this.three);
+			
+			//テストメソッド
 			this.game.action();
-
+			
+			//検証
+			mockKeybord.verify(() -> Keybord.getInt(this.one, this.three), times(3));
+		
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail();
@@ -87,12 +96,18 @@ public class QuizCuiGameApplicationImplTest{
 	/**
 	 * testCreateQuizList001 正常系
 	 * private void createQuizList() throws SystemException
-	 * 問題数が正常の3
-	 * listにQuizQuestionに情報を詰めてインスタンス化したものが追加されているかを確認
+	 * --確認事項--
+	 * listフィールドにそれぞれのデータが追加されたQuizQuestionインスタンスが3個追加されているか
+	 * --条件--
+	 * listフィールドの値は空のリスト
+	 * --検証項目--
+	 * 1.それぞれのインスタンスのフィールドにそれぞれのデータが追加されているか
+	 * 2. listの長さが3であるか
 	 */
 	@Test
 	public void testCreateQuizList001() {
 		try{
+			//準備
 			Method method = QuizCuiGameApplicationImpl.class.getDeclaredMethod("createQuizList");
 			Field listField = QuizCuiGameApplicationImpl.class.getDeclaredField("list");
 			method.setAccessible(true);
@@ -112,6 +127,7 @@ public class QuizCuiGameApplicationImplTest{
 				assertEquals(this.quizList.get(i).getProblemChoice(), list.get(i).getProblemChoice());
 				assertEquals(this.quizList.get(i).getCorrect(), list.get(i).getCorrect());
 			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail();
@@ -121,13 +137,20 @@ public class QuizCuiGameApplicationImplTest{
 	/**
 	 * testCreateQuizList002 異常系
 	 * private void createQuizList() throws SystemException
-	 * 問題数が0
-	 * listに何も追加されないことを確認
+	 * --確認事項--
+	 * listフィールドにそれぞれのデータが追加されたQuizQuestionインスタンスが0個追加されているか
+	 * --条件--
+	 * listフィールドの値は空のリスト
+	 * --検証項目--
+	 * 1. listの長さが0であるか
 	 */
 	@Test
 	public void testCreateQuizList002() {
+		//モック化
 		try(MockedStatic<MessageProperties> mockMessageProperties = mockStatic(MessageProperties.class)){
 			mockMessageProperties.when(() -> MessageProperties.getMessage("quiz.number.questions")).thenReturn(String.valueOf(this.zero));
+			
+			//準備
 			Method method = QuizCuiGameApplicationImpl.class.getDeclaredMethod("createQuizList");
 			Field listField = QuizCuiGameApplicationImpl.class.getDeclaredField("list");
 			method.setAccessible(true);
@@ -140,8 +163,9 @@ public class QuizCuiGameApplicationImplTest{
 			//検証
 			@SuppressWarnings("unchecked")
 			List<QuizQuestion> list = (List<QuizQuestion>) listField.get(this.game);
-			assertEquals(this.zero, list.size());
-		} catch (NullPointerException| SecurityException | NoSuchMethodException | IllegalAccessException | IllegalArgumentException  | ExceptionInInitializerError | NoSuchFieldException | InvocationTargetException  e) {
+			assertEquals(this.emptyQuizList.size(), list.size());
+			
+		} catch (Exception  e) {
 			e.printStackTrace();
 			fail();
 		}
@@ -150,13 +174,22 @@ public class QuizCuiGameApplicationImplTest{
 	/**
 	 * testCreateQuizList003 異常系
 	 * private void createQuizList() throws SystemException
-	 * 問題数が異常値
-	 * NumberFormatExceptionが投げられ、SystemExceptionが発生することを確認
+	 * --確認事項--
+	 * 問題数が異常値の場合、SystemExceptionが発生する
+	 * --条件--
+	 * listフィールドの値は空のリスト
+	 * --検証項目--
+	 * 1. SystemExceptionがスローされる
+	 * 2. メッセージ「プロパティの値が不正です。」が含まれているか (取り出せないため未検証)
+	 * 3. listフィールドの長さは0
 	 */
 	@Test
 	public void testCreateQuizList003() {
+		//モック化
 		try(MockedStatic<MessageProperties> mockMessageProperties = mockStatic(MessageProperties.class)){
 			mockMessageProperties.when(() -> MessageProperties.getMessage("quiz.number.questions")).thenReturn(this.alphabet);
+			
+			//準備
 			Method method = QuizCuiGameApplicationImpl.class.getDeclaredMethod("createQuizList");
 			Field listField = QuizCuiGameApplicationImpl.class.getDeclaredField("list");
 			method.setAccessible(true);
@@ -169,9 +202,10 @@ public class QuizCuiGameApplicationImplTest{
 			//検証
 			@SuppressWarnings("unchecked")
 			List<QuizQuestion> list = (List<QuizQuestion>) listField.get(this.game);
-			assertEquals(this.zero, list.size());
 			assertEquals(e.getTargetException().getClass(), SystemException.class);
-		} catch (NullPointerException| SecurityException | NoSuchMethodException | IllegalAccessException | IllegalArgumentException  | ExceptionInInitializerError | NoSuchFieldException e) {
+			assertEquals(this.emptyQuizList.size(), list.size());
+			
+		} catch (Exception e) {
 			e.printStackTrace();
 			fail();
 		}
@@ -180,15 +214,23 @@ public class QuizCuiGameApplicationImplTest{
 	/**
 	 * testViewProblem001 正常系
 	 * private void viewProblem(QuizQuestion quiz)
-	 * 引数のQuizQuestionインスタンスからtitle、body、choiceのゲッターが1回ずつ呼び出されることを確認
+	 * --確認事項--
+	 * title、body、choiceのゲッターが呼び出され、出力されるか
+	 * --条件--
+	 *	メソッドの引数はmock化したQuizQuestionクラス
+	 * --検証項目--
+	 * 1.title、body、choiceのゲッターが1回だけ呼び出されるか
 	 */
 	@Test
 	public void testViewProblem001() {
 		try{
-			QuizQuestion mockQuizQuestion = mock(QuizQuestion.class);
+			//準備
 			Method method = QuizCuiGameApplicationImpl.class.getDeclaredMethod("viewProblem", QuizQuestion.class);
 			method.setAccessible(true);
-
+			
+			//モック化
+			QuizQuestion mockQuizQuestion = mock(QuizQuestion.class);
+			
 			//テストメソッド
 			method.invoke(this.game, mockQuizQuestion);
 
@@ -196,7 +238,8 @@ public class QuizCuiGameApplicationImplTest{
 			verify(mockQuizQuestion, times(1)).getProblemTitle();
 			verify(mockQuizQuestion, times(1)).getProblemBody();
 			verify(mockQuizQuestion, times(1)).getProblemChoice();
-		} catch (NullPointerException| SecurityException | NoSuchMethodException | IllegalAccessException | IllegalArgumentException  | ExceptionInInitializerError | InvocationTargetException e) {
+			
+		} catch (Exception e) {
 			e.printStackTrace();
 			fail();
 		}
@@ -205,7 +248,14 @@ public class QuizCuiGameApplicationImplTest{
 	/**
 	 * testJudge001 正常系
 	 * private void judge(QuizQuestion quiz) throws SystemException
-	 * 一度不正な値を入力して再度入力が求められることと、正解数が増えているかを確認
+	 * --確認事項--
+	 * 不正な値が入力されたら再度入力が求められ、正常な値が入力されたら正解数を増やして終了する
+	 * --条件--
+	 *	getIntは1回目はApplicationExceptionを発生し、2回目は1が入力されるようにmock化
+	 * correctCountは0
+	 * --検証項目--
+	 * 1. correctカウントはインクリメントされるか
+	 * 2. getIntは2回呼び出されるか
 	 */
 	@Test
 	public void testJudge001() {
@@ -214,7 +264,8 @@ public class QuizCuiGameApplicationImplTest{
 			mockKeybord.when(() -> Keybord.getInt(this.one, this.three)).thenThrow(new ApplicationException(null)).thenReturn(this.one);
 			QuizQuestion mockQuizQuestion = mock(QuizQuestion.class);
 			when(mockQuizQuestion.getCorrect()).thenReturn(this.one);
-
+			
+			//準備
 			Method method = QuizCuiGameApplicationImpl.class.getDeclaredMethod("judge", QuizQuestion.class);
 			Field correctCountField = QuizCuiGameApplicationImpl.class.getDeclaredField("correctConut");
 			method.setAccessible(true);
@@ -228,27 +279,40 @@ public class QuizCuiGameApplicationImplTest{
 			int correctCount = (int) correctCountField.get(this.game);
 			assertEquals(this.one, correctCount);
 			verify(mockQuizQuestion, times(2)).getCorrect();
-		} catch (NullPointerException| SecurityException | NoSuchMethodException | IllegalAccessException | IllegalArgumentException  | ExceptionInInitializerError | NoSuchFieldException | InvocationTargetException e) {
+			
+		} catch (Exception e) {
 			e.printStackTrace();
 			fail();
 		}
 	}
-
 	/**
 	 * testViewResult001 正常系
 	 * private void viewResult() throws SystemException
-	 * 例外が発生しないことを確認
+	 * --確認事項--
+	 * 正解数に応じてメッセージが出力される
+	 * --条件--
+	 *	correctCountフィールドは0
+	 * --検証項目--
+	 * 1. correctCountフィールドに変更がないか
+	 * 2. 例外が発生せずに終了するか
 	 */
 	@Test
 	public void testViewResult001() {
 		try{
+			//準備
 			Method method = QuizCuiGameApplicationImpl.class.getDeclaredMethod("viewResult");
+			Field correctCountField = QuizCuiGameApplicationImpl.class.getDeclaredField("correctConut");
 			method.setAccessible(true);
+			correctCountField.setAccessible(true);
+			correctCountField.set(this.game, this.zero);
 
 			//テストメソッド
 			method.invoke(this.game);
 
 			//検証
+			int correctCount = (int) correctCountField.get(this.game);
+			assertEquals(this.zero, correctCount);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail();
